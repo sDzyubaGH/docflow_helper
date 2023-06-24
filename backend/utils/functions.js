e0d59a5ae9862992b662074e75d8033fc1218d23
Sergey <ser.dzuba@mail.ru>
2023-06-24 14:49:47 +0300

    locale task status

--- backend/utils/functions.js
+++ backend/utils/functions.js
@@ -6,3 +6,18 @@ export function localeDate(date) {
   const [year, month, day] = date.split('-')
   return `${day}.${month}.${year}`
 }
+
+export function localeTaskStatus(status) {
+  if (!status) {
+    return null
+  }
+
+  switch (status) {
+    case "Задача": {
+      return 'Не завершена'
+    }
+    case "Задача закрыта": {
+      return "Завершена"
+    }
+  }
+}
--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,12 +1,13 @@
 import React from "react"
-import {localeDate} from "../utils/functions"
+import { localeDate } from "../utils/functions"
+import { localeTaskStatus } from "../../../backend/utils/functions"
 
-export default function Task({task}) {
+export default function Task({ task }) {
   return (
     <tr className="border-b">
       <td className="text-center">{localeDate(task?.date_route) || "–"}</td>
       <td className="text-center py-2">{task?.number_source || "–"}</td>
-      <td className="text-center whitespace-nowrap">{task?.task_type || "–"}</td>
+      <td className="text-center whitespace-nowrap">{localeTaskStatus(task?.task_type) || "–"}</td>
       <td className="text-left">{task?.task_text || "–"}</td>
       <td className="text-center">{localeDate(task?.date_source) || "–"}</td>
       <td className="text-center">{localeDate(task?.date_received) || "–"}</td>
